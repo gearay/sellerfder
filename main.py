a4b6e2632d728c63f7aa18da94a6f54b80077cec
Yan Chen <cheny.gary@gmail.com>
2017-12-15 16:39:00 +0800

    add page number prompt

--- main.py
+++ main.py
@@ -10,25 +10,25 @@ import json
 from functools import reduce
 
 
-def fdseller(keywords):
+def fdseller(keywords,n):
     sellers = []
     # print(keywords[0])
     for item in keywords:
 
         seller = set()
-        for page in range(0, 750, 15):
+        for page in range(0,15*n, 15):
             postdata = {
                 'q': item,
                 'js': '1',
                 'ie': 'utf8',
                 'uniq': 'shop',
-                'sort': 'credit-desc',
+                'sort': 'price-asc',
                 's': page
 
             }
             postdata = urllib.urlencode(postdata)
             taobao = "https://s.taobao.com/search?" + postdata
-            # print(taobao)
+            print(taobao)
             try:
                 content1 = getHtml(taobao)
                 content1 = content1.decode('utf-8', 'ignore')
@@ -36,6 +36,7 @@ def fdseller(keywords):
                     r'g_page_config = (.*?);\n', content1, re.S)
                 product = json.loads(content1[0])
                 productfiles = product['mods']['itemlist']['data']['sellers']
+                #print(productfiles)
                 for productfile in productfiles:
                     seller.add(productfile['user_id'])
 
@@ -43,18 +44,20 @@ def fdseller(keywords):
                 if hasattr(e, 'code'):
                     print('页面不存在或时间太长.')
                     print('Error code:', e.code)
-                raise e
+                #raise e
             finally:
                 next
-        # print(seller)
+        #print(seller)
         sellers.append(seller)
     # return(sellers)
     return reduce(lambda x, y: x & y, sellers)
 
 if __name__ == "__main__":
+    pagen = raw_input("需要查几页")
+    pagen = int(pagen)
     keys = []
-    for keyword in iter(raw_input, ''):
+    for keyword in iter(raw_input,''):
         keys.append(keyword)
-    fdsellers = fdseller(keys)
+    fdsellers = fdseller(keys,pagen)
     for fd in fdsellers:
         print('https://store.taobao.com/shop/view_shop.htm?user_number_id=%s' % fd)
